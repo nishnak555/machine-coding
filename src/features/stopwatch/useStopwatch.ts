447937d7c51b2f775c2cdd339116d7a85837e8f8
Nishank <pathaknishank007@gmail.com>
2025-11-26 09:44:17 +0530

    update stopwatch hook

--- src/features/stopwatch/useStopwatch.ts
+++ src/features/stopwatch/useStopwatch.ts
@@ -1,33 +1,32 @@
+import { useState, useRef } from "react";
 
-import { useRef, useState } from "react"
-export const useStopwatch = () =>{
-const timerRef =   useRef<NODEJS.Timeout |null>(null)  
-const[time,setTime]  = useState<number>(0)
+export const useStopwatch = () => {
+  const [time, setTime] = useState<number>(0);
+  const timerRef = useRef<number | null>(null);
 
-const start = ()=>{
-  timerRef.current =  setInterval(()=>{
-setTime((t)=>t+1)
-  },1000)
-}
+  const start = () => {
+    if (timerRef.current) return;
+    timerRef.current = window.setInterval(() => {
+      setTime((t) => t + 1);
+    }, 1000);
+  };
 
-const pause = ()=>{
-  clearInterval(timerRef.current)
-  timerRef.current = null
-}
+  const pause = () => {
+    if (timerRef.current) clearInterval(timerRef.current);
+    timerRef.current = null;
+  };
 
-const reset = ()=>{
-  pause()
-  setTime(0)
-}
+  const reset = () => {
+    pause();
+    setTime(0);
+  };
 
+  const reverse = () => {
+    pause();
+    timerRef.current = window.setInterval(() => {
+      setTime((t) => (t > 0 ? t - 1 : 0));
+    }, 1000);
+  };
 
-const reverse = () =>{
-  pause()
-  timerRef.current = setInterval(() => {
-    setTime((t)=>(t > 0 ? t-1 : 0))
-  },1000);
-}
-return {start,time,pause,reset,reverse}
-}
-
-
+  return { time, start, pause, reset, reverse };
+};
